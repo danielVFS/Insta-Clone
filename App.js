b3e1a7bbd3334e4a9b6e9852d3f8812c2e0a187e
danielVFS <danielv759153@gmail.com>
2020-06-08 21:40:41 -0300

    Configurando pastas do projeto & eslint/prettier

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Interface from './src/components/Interface';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Interface />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
